589a4a69652708359bf9982e1b47ee0d732f986b
littlecapa <littlecapa@googlemail.com>
2024-03-18 19:27:57 +0100

    new homepage

---
+++ myportfolio/urls.py
@@ -0,0 +1,8 @@
+# myportfolio/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,3 +1,6 @@
+# myportfolio/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'myportfolio/home.html')
