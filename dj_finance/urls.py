5e4ab84a668106915642bcdd38dbbc7aa12659df
littlecapa <littlecapa@googlemail.com>
2024-03-19 12:03:37 +0100

    Improved URL Pathes

--- dj_finance/urls.py
+++ dj_finance/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myportfolio.urls')),
+    path('', RedirectView.as_view(url='/myportfolio/', permanent=True)),  # Redirect base URL to /y/a/
+    path('myportfolio/', include('myportfolio.urls')),
 ]
